3c826a240d333795a09e9b1d200282fa478a0c2b
Edward Rowe <edward.l.rowe@gmail.com>
2016-11-24 13:30:53 -0500

    Add initial project

---
+++ Assets/Editor/RBPackageExporter.cs
@@ -0,0 +1,218 @@
+﻿/* MIT License
+
+Copyright (c) 2016 Edward Rowe, RedBlueGames
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Tool that helps us export RedBlueTools for use in other projects, as well as for the public
+/// </summary>
+public class RBPackageExporter : UnityEditor.EditorWindow
+{
+    private static string companyPath = "Assets/RedBlueGames";
+    private static string packageExtension = ".unitypackage";
+    private static string testPackageSuffix = "WithTests";
+    private List<RBAsset> redBlueAssets;
+
+    private bool includeTests;
+
+    [MenuItem("Assets/RBPackage Exporter")]
+    private static void ExportRBScriptsWithTests()
+    {
+        EditorWindow.GetWindow<RBPackageExporter>(false, "RBPackage Exporter", true);
+    }
+
+    private static List<string> GetTestDirectories(string[] directories)
+    {
+        var testDirectories = new List<string>();
+        foreach (var directory in directories)
+        {
+            if (IsDirectoryAChildOfAnyOfThese(directory, testDirectories))
+            {
+                testDirectories.Add(directory);
+                continue;
+            }
+
+            if (IsTestDirectory(directory))
+            {
+                testDirectories.Add(directory);
+            }
+        }
+
+        return testDirectories;
+    }
+
+    private static bool IsDirectoryAChildOfAnyOfThese(string path, List<string> possibleParentDirectories)
+    {
+        foreach (var possibleParentDirectory in possibleParentDirectories)
+        {
+            if (path.Contains(possibleParentDirectory))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static bool IsTestDirectory(string path)
+    {
+        return System.IO.Path.GetFileName(path) == "Tests";
+    }
+
+    private void OnEnable()
+    {
+        this.redBlueAssets = new List<RBAsset>();
+        this.FindAssetsInCompanyFolder();
+    }
+
+    private void FindAssetsInCompanyFolder()
+    {
+        foreach (var subdirectory in System.IO.Directory.GetDirectories(companyPath))
+        {
+            var splitSubdirectory = subdirectory.Split(System.IO.Path.DirectorySeparatorChar);
+            string folderName = splitSubdirectory[splitSubdirectory.Length - 1];
+            this.redBlueAssets.Add(new RBAsset()
+                {
+                    AssetName = folderName,
+                    IsSelected = false
+                });
+        }
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.HelpBox(
+            "This tool Allows quick export of specific RedBlueGames custom assets. It also allows optional export of Tests folders.",
+            MessageType.None);
+        EditorGUILayout.LabelField("Asset Packages to export:", EditorStyles.boldLabel);
+        EditorGUILayout.BeginVertical();
+        for (int i = 0; i < this.redBlueAssets.Count; i++)
+        {
+            this.redBlueAssets[i].IsSelected = EditorGUILayout.Toggle(
+                this.redBlueAssets[i].AssetName,
+                this.redBlueAssets[i].IsSelected);
+        }
+
+        EditorGUILayout.EndVertical();
+
+        // Check if any assets are selected.
+        bool atLeastOnePackageSelected = false;
+        foreach (var assetPackage in this.redBlueAssets)
+        {
+            if (assetPackage.IsSelected)
+            {
+                atLeastOnePackageSelected = true;
+                break;
+            }
+        }
+
+        EditorGUILayout.Separator();
+        this.includeTests = EditorGUILayout.Toggle("Include Tests", this.includeTests);
+
+        EditorGUI.BeginDisabledGroup(!atLeastOnePackageSelected);
+        if (GUILayout.Button("Export"))
+        {
+            this.ExportAllPackages(this.includeTests);
+        }
+
+        EditorGUI.EndDisabledGroup();
+
+        if (!atLeastOnePackageSelected)
+        {
+            EditorGUILayout.HelpBox(
+                "No packages selected to export. Select at least one asset Package.",
+                MessageType.Warning);
+        }
+    }
+
+    private void ExportAllPackages(bool includeTests)
+    {
+        foreach (var asset in this.redBlueAssets)
+        {
+            if (asset.IsSelected)
+            {
+                this.ExportRBScripts(asset, includeTests);
+            }
+        }
+    }
+
+    private void ExportRBScripts(RBAsset assetToExport, bool includeTests)
+    {
+        var subDirectories = System.IO.Directory.GetDirectories(companyPath, "*", System.IO.SearchOption.AllDirectories);
+        var directoriesToExport = new List<string>(subDirectories);
+
+        var testDirectories = GetTestDirectories(subDirectories);
+        if (!includeTests)
+        {
+            foreach (var testDirectory in testDirectories)
+            {
+                directoriesToExport.Remove(testDirectory);
+            }
+        }
+
+        // Do not export the other packages
+        foreach (var asset in this.redBlueAssets)
+        {
+            if (assetToExport.AssetName != asset.AssetName)
+            {
+                string assetPath = companyPath + System.IO.Path.DirectorySeparatorChar + asset.AssetName;
+                directoriesToExport.Remove(assetPath);
+
+                var subdirectoriesOfAsset = System.IO.Directory.GetDirectories(assetPath, "*", System.IO.SearchOption.AllDirectories);
+                foreach (var subdirectory in subdirectoriesOfAsset)
+                {
+                    directoriesToExport.Remove(subdirectory);
+                }
+            }
+        }
+
+        var allAssetPaths = new List<string>();
+        foreach (var directory in directoriesToExport)
+        {
+            var filesInDirectory = System.IO.Directory.GetFiles(directory);
+            allAssetPaths.AddRange(filesInDirectory); 
+        }
+
+        if (allAssetPaths.Count == 0)
+        {
+            Debug.Log("No assets to export. Will not export asset package: " + assetToExport.AssetName);
+            return;
+        }
+
+        string filename = string.Concat(assetToExport.AssetName, includeTests ? testPackageSuffix : string.Empty, packageExtension);
+        AssetDatabase.ExportPackage(
+            allAssetPaths.ToArray(),
+            filename,
+            ExportPackageOptions.IncludeDependencies | ExportPackageOptions.Interactive);
+    }
+
+    private class RBAsset
+    {
+        public string AssetName { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
---
+++ Assets/RedBlueGames/BulkRename/BulkRename.cs
@@ -0,0 +1,204 @@
+﻿/* MIT License
+
+Copyright (c) 2016 Edward Rowe, RedBlueGames
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+namespace RedBlueGames.Tools
+{
+    using System.Collections.Generic;
+    using UnityEditor;
+    using UnityEngine;
+
+    /// <summary>
+    /// Tool that tries to allow renaming mulitple selections by parsing similar substrings
+    /// </summary>
+    public class BulkRename : EditorWindow
+    {
+        private const string MenuPath = "Assets/Rename In Bulk";
+
+        private List<UnityEngine.Object> objectsToRename;
+
+        private string prefix;  
+        private string suffix;
+        private string searchToken;
+        private string replacementString;
+
+        [MenuItem(MenuPath)]
+        private static void ShowRenameSpritesheetWindow()
+        {
+            EditorWindow.GetWindow<BulkRename>(true, "Bulk Rename", true);
+        }
+
+        [MenuItem(MenuPath, true)]
+        private static bool IsSelectionValid()
+        {
+            if (Selection.activeObject == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool ObjectIsValidForRename(UnityEngine.Object obj)
+        {
+            return AssetDatabase.Contains(obj);
+        }
+
+        private void OnEnable()
+        {
+            Selection.selectionChanged += this.Repaint;
+
+            this.prefix = string.Empty;
+            this.suffix = string.Empty;
+            this.searchToken = string.Empty;
+            this.replacementString = string.Empty;
+
+            this.RefreshObjectsToRename();
+        }
+
+        private void OnDisable()
+        {
+            Selection.selectionChanged -= this.Repaint;
+        }
+
+        private void RefreshObjectsToRename()
+        {
+            var selectedObjects = Selection.objects;
+            this.objectsToRename = new List<UnityEngine.Object>();
+            foreach (var selectedObject in selectedObjects)
+            {
+                if (ObjectIsValidForRename(selectedObject))
+                {
+                    this.objectsToRename.Add(selectedObject);
+                }
+            }
+        }
+
+        private void OnGUI()
+        {
+            this.RefreshObjectsToRename();
+
+            EditorGUILayout.HelpBox(
+                "The Bulk Rename tool tries to allow renaming mulitple selections by parsing similar substrings",
+                MessageType.None);
+
+            EditorGUILayout.Space();
+
+            if (this.objectsToRename.Count == 0)
+            {
+                EditorGUILayout.HelpBox("No objects selected. Select some objects to rename.", MessageType.Error);
+                return;
+            }
+
+            EditorGUILayout.Space();
+
+            EditorGUILayout.LabelField("Additions", EditorStyles.boldLabel);
+            this.prefix = EditorGUILayout.TextField("Prefix", this.prefix);
+            this.suffix = EditorGUILayout.TextField("Suffix", this.suffix);
+
+            EditorGUILayout.LabelField("Text Replacement", EditorStyles.boldLabel);
+
+            this.searchToken = EditorGUILayout.TextField("Search Token", this.searchToken);
+            this.replacementString = EditorGUILayout.TextField("Replace with", this.replacementString);
+
+            if (GUILayout.Button("Rename"))
+            {
+                this.RenameAssets();
+                this.Close();
+            }
+
+            EditorGUILayout.Space();
+            GUILayout.Box(string.Empty, new GUILayoutOption[] { GUILayout.ExpandWidth(true), GUILayout.Height(1) });
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Original Name", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("New Name", EditorStyles.boldLabel);
+            EditorGUILayout.EndHorizontal();
+
+            foreach (var objectToRename in this.objectsToRename)
+            {
+                EditorGUILayout.BeginHorizontal();
+
+                var objectName = objectToRename.name;
+                var previewObjectname = this.GetNewNameForAsset(objectToRename);
+                EditorGUILayout.LabelField(objectName);
+
+                bool namesDiffer = previewObjectname != objectName;
+                var style = namesDiffer ? EditorStyles.boldLabel : new GUIStyle(EditorStyles.label);
+                EditorGUILayout.LabelField(previewObjectname, style);
+
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+
+        private void RenameAssets()
+        {
+            for (int i = 0; i < this.objectsToRename.Count; ++i)
+            {
+                var asset = this.objectsToRename[i];
+
+                var infoString = string.Format(
+                                     "Renaming asset {0} of {1}",
+                                     i,
+                                     this.objectsToRename.Count);
+
+                EditorUtility.DisplayProgressBar(
+                    "Renaming Assets...",
+                    infoString,
+                    i / (float)this.objectsToRename.Count);
+                
+                this.RenameAsset(asset);
+            }
+
+            EditorUtility.ClearProgressBar();
+        }
+
+        private void RenameAsset(UnityEngine.Object asset)
+        {
+            var pathToAsset = AssetDatabase.GetAssetPath(asset);
+            var newName = this.GetNewNameForAsset(asset);
+            AssetDatabase.RenameAsset(pathToAsset, newName);
+        }
+
+        private string GetNewNameForAsset(UnityEngine.Object asset)
+        {
+            var modifiedName = asset.name;
+
+            // Replace strings first so we don't replace the prefix.
+            if (!string.IsNullOrEmpty(this.searchToken))
+            {
+                modifiedName = asset.name.Replace(this.searchToken, this.replacementString);
+            }
+
+            if (!string.IsNullOrEmpty(this.prefix))
+            {
+                modifiedName = string.Concat(this.prefix, modifiedName);
+            }
+
+            if (!string.IsNullOrEmpty(this.suffix))
+            {
+                modifiedName = string.Concat(modifiedName, this.suffix);
+            }
+
+            return modifiedName;
+        }
+    }
+}
