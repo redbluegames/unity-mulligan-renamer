51cdb49d76aebe7edda154d66b9f5446795bfc95
Edward Rowe <edward.l.rowe@gmail.com>
2017-06-20 01:54:57 -0400

    Add simple version of RemoveCharactersOp
    The complex version would supply presets from a dropdown, or allow
    users to configure it with a Custom option. Presets would be 'Symbols'
    and 'Numbers'. I abandoned this when it increased complexity by about
    tenfold. May return to it if I can find a way to simplify them.

---
+++ Assets/RedBlueGames/BulkRename/Tests/Editor/RemoveCharactersOpTests.cs
@@ -0,0 +1,43 @@
+﻿namespace RedBlueGames.BulkRename.Tests
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using NUnit.Framework;
+
+    public class RemoveCharactersOpTests
+    {
+        [Test]
+        public void RemoveCharacters_EmptyTarget_IsUnchanged()
+        {
+            // Arrange
+            var name = string.Empty;
+            var removeCharactersOp = new RemoveCharactersOperation();
+
+            var expected = string.Empty;
+
+            // Act
+            string result = removeCharactersOp.Rename(name, 0);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void RemoveCharacters_RemoveSymbols_RemovesOnlySymbols()
+        {
+            // Arrange
+            var name = "A!@#$%BD*(";
+            var removeCharactersOp = new RemoveCharactersOperation();
+            removeCharactersOp.Characters = "!@#$%^&*()";
+
+            var expected = "ABD";
+
+            // Act
+            string result = removeCharactersOp.Rename(name, 0);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
