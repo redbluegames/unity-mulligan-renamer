b3d1603a3f93c1a9bd192903f4d0a7ed316b00b0
Edward Rowe <edward.l.rowe@gmail.com>
2017-09-03 11:36:06 -0400

    Add missing namespaces and license boilerplate

--- Assets/RedBlueGames/MulliganRenamer/Editor/GameObjectExtensions.cs
+++ Assets/RedBlueGames/MulliganRenamer/Editor/GameObjectExtensions.cs
@@ -1,47 +1,73 @@
-﻿using UnityEditor;
-using UnityEngine;
+﻿/* MIT License
 
-/// <summary>
-/// Extensions for the GameObject class
-/// </summary>
-public static class GameObjectExtensions
+Copyright (c) 2016 Edward Rowe, RedBlueGames
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+namespace RedBlueGames.MulliganRenamer
 {
-    /// <summary>
-    /// Gets the hierarchy sorting for the GameObject. This is like Sibling index only weighted based on the
-    /// position of the object in the hierarchy. It should match the way it displays in the Unity scene.
-    /// </summary>
-    /// <returns>The hierarchy sorting of the GameObject.</returns>
-    /// <param name="obj">GameObject to get the sorting for.</param>
-    public static float GetHierarchySorting(this GameObject obj)
-    {
-        if (obj.transform.parent == null)
-        {
-            return obj.transform.GetSiblingIndex();
-        }
-        else
-        {
-            // Sorting =  parent.Sorting + siblingWeight * (siblingIndex + 1)
-            // A = 0
-            //  - a1 0 + (0.34 * 1) = 0.34
-            //    - a2 0.34 + (0.17 * 1) = 0.51
-            //  - b1 0 + (0.34 * 2) = 0.64
-            // B = 1
-            float weightFromSiblingIndex = obj.transform.GetSiblingWeight() * (obj.transform.GetSiblingIndex() + 1);
-            return obj.transform.parent.gameObject.GetHierarchySorting() + weightFromSiblingIndex;
-        }
-    }
+    using UnityEditor;
+    using UnityEngine;
 
-    private static float GetSiblingWeight(this Transform t)
+    /// <summary>
+    /// Extensions for the GameObject class
+    /// </summary>
+    public static class GameObjectExtensions
     {
-        if (t.parent == null)
+        /// <summary>
+        /// Gets the hierarchy sorting for the GameObject. This is like Sibling index only weighted based on the
+        /// position of the object in the hierarchy. It should match the way it displays in the Unity scene.
+        /// </summary>
+        /// <returns>The hierarchy sorting of the GameObject.</returns>
+        /// <param name="obj">GameObject to get the sorting for.</param>
+        public static float GetHierarchySorting(this GameObject obj)
         {
-            return 1;
+            if (obj.transform.parent == null)
+            {
+                return obj.transform.GetSiblingIndex();
+            }
+            else
+            {
+                // Sorting =  parent.Sorting + siblingWeight * (siblingIndex + 1)
+                // A = 0
+                //  - a1 0 + (0.34 * 1) = 0.34
+                //    - a2 0.34 + (0.17 * 1) = 0.51
+                //  - b1 0 + (0.34 * 2) = 0.64
+                // B = 1
+                float weightFromSiblingIndex = obj.transform.GetSiblingWeight() * (obj.transform.GetSiblingIndex() + 1);
+                return obj.transform.parent.gameObject.GetHierarchySorting() + weightFromSiblingIndex;
+            }
         }
-        else
+
+        private static float GetSiblingWeight(this Transform t)
         {
-            // SiblingWeight is the product of the weight of the parent divided amongst their children.
-            // An element with 5 siblings would have a sibling weight of .2.
-            return t.parent.GetSiblingWeight() / (t.parent.childCount + 1);
+            if (t.parent == null)
+            {
+                return 1;
+            }
+            else
+            {
+                // SiblingWeight is the product of the weight of the parent divided amongst their children.
+                // An element with 5 siblings would have a sibling weight of .2.
+                return t.parent.GetSiblingWeight() / (t.parent.childCount + 1);
+            }
         }
     }
 }
