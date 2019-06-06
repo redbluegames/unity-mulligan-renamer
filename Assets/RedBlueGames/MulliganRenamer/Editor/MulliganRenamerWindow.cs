71ba325347ee28fb0dee8cf37c06dbfab9105dc8
Edward Rowe <edward.l.rowe@gmail.com>
2019-06-06 15:01:37 -0400

    Tick up version to 1.7.0

--- Assets/RedBlueGames/MulliganRenamer/Editor/MulliganRenamerWindow.cs
+++ Assets/RedBlueGames/MulliganRenamer/Editor/MulliganRenamerWindow.cs
@@ -34,7 +34,7 @@ namespace RedBlueGames.MulliganRenamer
     /// </summary>
     public class MulliganRenamerWindow : EditorWindow
     {
-        private const string VersionString = "1.6.0";
+        private const string VersionString = "1.7.0";
         private const string WindowMenuPath = "Window/Red Blue/Mulligan Renamer";
 
         private const string RenameOpsEditorPrefsKey = "RedBlueGames.MulliganRenamer.RenameOperationsToApply";
