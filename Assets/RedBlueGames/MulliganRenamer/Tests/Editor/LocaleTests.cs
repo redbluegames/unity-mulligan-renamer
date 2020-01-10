82a6cee64e6d7ca1d54d697a8b3d5a283565aab9
Murillo Pugliesi <mukarillo@gmail.com>
2020-01-10 12:41:39 +0100

    fix #230 - adding some tests for the locale manager

---
+++ Assets/RedBlueGames/MulliganRenamer/Tests/Editor/LocaleTests.cs
@@ -0,0 +1,80 @@
+﻿/* MIT License
+
+Copyright (c) 2020 Murillo Pugliesi Lopes, https://github.com/Mukarillo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+namespace RedBlueGames.MulliganRenamer
+{
+    using NUnit.Framework;
+    using System;
+
+    public class LocaleTests
+    {
+        [Test]
+        public void CheckAllLanguageKeys_WithEnglish()
+        {
+            var englishLanguage = LocaleManager.Instance.AllLanguages.Find(x => x.LanguageKey == "en");
+
+            foreach (var language in LocaleManager.Instance.AllLanguages)
+            {
+                if(language.LanguageKey == "en")
+                    continue;
+
+                foreach (var englishElement in englishLanguage.Elements)
+                {
+                    if(!language.Elements.Exists(x => x.Key == englishElement.Key))
+                        throw new Exception($"Language {language.LanguageName} does not contains key {englishElement.Key}.");
+                }
+            }
+        }
+
+        [Test]
+        public void CheckAllLanguages_HasValue()
+        {
+            foreach (var language in LocaleManager.Instance.AllLanguages)
+            {
+                foreach (var element in language.Elements)
+                {
+                    if (string.IsNullOrEmpty(element.Value))
+                        throw new Exception($"Language {language.LanguageName} does not contains value in key {element.Key}.");
+                }
+            }
+        }
+
+        [Test]
+        public void ChangeLanguage()
+        {
+            var savedLanguage = LocaleManager.Instance.CurrentLanguage;
+            foreach (var language in LocaleManager.Instance.AllLanguages)
+            {
+                LocaleManager.Instance.ChangeLocale(language.LanguageKey);
+
+                if (!language.LanguageKey.Equals(LocaleManager.Instance.CurrentLanguage.LanguageKey))
+                {
+                    LocaleManager.Instance.ChangeLocale(savedLanguage.LanguageKey);
+                    throw new Exception();
+                }
+            }
+
+            LocaleManager.Instance.ChangeLocale(savedLanguage.LanguageKey);
+        }
+    }
+}
